d0eb885f083372435e5dc0c9689fa73a5744a36b
rinat shelkov <rinarserov@icloud.com>
2024-01-26 22:33:02 +0500

    Homework_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,12 +1,6 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE employees
-(
-	customer_id varchar(5) PRIMARY KEY,
-	company_name text NOT NULL,
-	contact_name text NOT NULL
-);
 
-CREATE TABLE customers
+CREATE TABLE employees
 (
 	employee_id int PRIMARY KEY,
 	first_name text NOT NULL,
@@ -17,12 +11,23 @@ CREATE TABLE customers
 
 );
 
+
+CREATE TABLE customers
+
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name text NOT NULL,
+	contact_name text NOT NULL
+);
+
+
 CREATE TABLE orders
 (
 	order_id int PRIMARY KEY,
-	customer_id varchar(5) UNIQUE  REFERENCES employees(customer_id),
-	employee_id int UNIQUE  REFERENCES customers(employee_id),
+	customer_id varchar(5)  REFERENCES customers(customer_id),
+	employee_id int  REFERENCES employees(employee_id),
 	order_date date NOT NULL,
 	ship_city text NOT NULL
 
 );
+
